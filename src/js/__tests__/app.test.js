8886ba365728062c1b344d4a6335e7894df6e0b4
Svetlana Kutyova <kutyovas@yandex.ru>
2021-10-26 21:55:36 +0300

    3

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,19 +1,20 @@
 import orderObject from '../app.js';
 
-test('should person', () => {
-  const heroes = [
-    { name: 'мечник', health: 10 },
-    { name: 'маг', health: 100 },
-    { name: 'лучник', health: 80 },
-  ];
-  const output = [
-    { name: 'маг', health: 100 },
-    { name: 'лучник', health: 80 },
-    { name: 'мечник', health: 10 },
-  ];
-  const result = orderObject(heroes);
-  expect(() => {
-    expect(result).toBe(output);
-  }).toThrow();
+const heroes = [
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+];
+const output = [
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+  { name: 'мечник', health: 10 },
+];
+const result = orderObject(heroes);
+
+test('have not all the same properties', () => {
+  expect(result).not.toBe(output);
+});
+test('person have all the same properties', () => {
   expect(result).toEqual(output);
 });
